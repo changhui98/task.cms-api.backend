08cf6596f7c20e73a2175b53c3826dca4cb1072a
changhui98 <net_0102@naver.com>
2026-03-07 17:34:18 +0900

    Feat : 회원가입 진행 시 비밀번호 암호화 하기 위해 빈 등록

--- src/main/java/com/malgn/global/configure/AppConfiguration.java
+++ src/main/java/com/malgn/global/configure/AppConfiguration.java
@@ -2,9 +2,17 @@ package com.malgn.global.configure;
 
 import lombok.RequiredArgsConstructor;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @RequiredArgsConstructor
 @Configuration
 public class AppConfiguration {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
