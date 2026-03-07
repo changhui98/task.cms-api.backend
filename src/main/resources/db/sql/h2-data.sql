e5eb3883db0da8d63c01ef4b5a3a3947832d368a
changhui98 <net_0102@naver.com>
2026-03-07 21:59:56 +0900

    Style : 회원가입시 필요한 데이터 필드 맞춰 더미 데이터 필드 수정

--- src/main/resources/db/sql/h2-data.sql
+++ src/main/resources/db/sql/h2-data.sql
@@ -1,3 +1,3 @@
 -- TODO data
-insert into members (name, password, role, last_modified_date) values('bob', 'test1234', 'ADMIN' , now());
+insert into members (username, password, role, last_modified_date) values('bob', 'test1234', 'ADMIN' , now());
 -- example
