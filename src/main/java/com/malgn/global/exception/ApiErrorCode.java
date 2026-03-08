1ea3b9aab78073fe22a3842c7b16e418e5dad070
changhui98 <net_0102@naver.com>
2026-03-08 16:56:07 +0900

    Style : 잘못된 요청은 BAD_REQUEST로 status 값 변경

--- src/main/java/com/malgn/global/exception/ApiErrorCode.java
+++ src/main/java/com/malgn/global/exception/ApiErrorCode.java
@@ -8,7 +8,7 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum ApiErrorCode implements ErrorCode {
 
-    INVALID_REQUEST(HttpStatus.FORBIDDEN, "A001", "잘못된 요청입니다."),
+    INVALID_REQUEST(HttpStatus.BAD_REQUEST, "A001", "잘못된 요청입니다."),
     FORBIDDEN(HttpStatus.FORBIDDEN, "A002", "접근 권한이 없습니다."),
     UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "A003", "인증이 필요합니다.")
 
