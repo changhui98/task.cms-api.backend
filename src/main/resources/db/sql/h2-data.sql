dbb2673c903318bfe0009ce89b1e8b17dc26f926
changhui98 <net_0102@naver.com>
2026-03-08 14:12:11 +0900

    Style : 초기 데이터 어드민과 유저 설정

--- src/main/resources/db/sql/h2-data.sql
+++ src/main/resources/db/sql/h2-data.sql
@@ -1,3 +1,4 @@
 -- TODO data
-insert into members (username, password, role, last_modified_date) values('bob', '$2a$10$CDJz0aKY40qnIbmK/ANLEO8SE/6jWLSDy1.AE9p.N1YTaWYQKT2o6', 'ADMIN' , now());
+insert into members (username, password, role, last_modified_date) values('user', '$2a$10$CDJz0aKY40qnIbmK/ANLEO8SE/6jWLSDy1.AE9p.N1YTaWYQKT2o6', 'USER' , now());
+insert into members (username, password, role, last_modified_date) values('admin', '$2a$10$CDJz0aKY40qnIbmK/ANLEO8SE/6jWLSDy1.AE9p.N1YTaWYQKT2o6', 'ADMIN' , now());
 -- example
