f66aa9e98b9eb94534a6b06f0c241cff52a59d73
changhui98 <net_0102@naver.com>
2026-03-08 21:44:06 +0900

    Style : 중복된 유저 Status 값 수정

--- src/main/java/com/malgn/member/domain/MemberErrorCode.java
+++ src/main/java/com/malgn/member/domain/MemberErrorCode.java
@@ -10,7 +10,7 @@ import org.springframework.http.HttpStatus;
 public enum MemberErrorCode implements ErrorCode {
 
     MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "U001", "조회하려는 사용자가 존재하지 않습니다."),
-    DUPLICATE_USERNAME(HttpStatus.BAD_REQUEST, "U002", "이미 존재하는 아이디입니다."),
+    DUPLICATE_USERNAME(HttpStatus.CONFLICT, "U002", "이미 존재하는 아이디입니다."),
     MEMBER_UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "U003", "인증되지 않은 사용자입니다."),
     INVALID_CREDENTIALS(HttpStatus.BAD_REQUEST, "U004", "아이디 또는 비밀번호가 올바르지 않습니다.")
 
