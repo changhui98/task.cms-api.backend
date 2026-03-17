083c9370476d40d6eecf9d8e082d279b2fb90c2e
changhui98 <net_0102@naver.com>
2026-03-17 21:33:45 +0900

    Feat : 사용자 탈퇴 기능 구현

--- src/main/java/com/malgn/member/application/service/MemberService.java
+++ src/main/java/com/malgn/member/application/service/MemberService.java
@@ -1,8 +1,13 @@
 package com.malgn.member.application.service;
 
+import com.malgn.global.configure.CustomUser;
+import com.malgn.global.exception.AppException;
+import com.malgn.member.domain.MemberErrorCode;
+import com.malgn.member.domain.entity.Member;
 import com.malgn.member.domain.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -10,4 +15,21 @@ public class MemberService {
 
     private final MemberRepository memberRepository;
 
+    @Transactional
+    public void deleteMember(CustomUser user) {
+
+        Member member = getMember(user);
+
+        if (member.isDeleted()) {
+            throw new AppException(MemberErrorCode.MEMBER_NOT_FOUND);
+        }
+
+        member.delete();
+    }
+
+    private Member getMember(CustomUser user) {
+        return memberRepository.findByUsername(user.getUsername()).orElseThrow(
+            () -> new AppException(MemberErrorCode.MEMBER_NOT_FOUND)
+        );
+    }
 }
--- src/main/java/com/malgn/member/presentation/controller/MemberController.java
+++ src/main/java/com/malgn/member/presentation/controller/MemberController.java
@@ -1,7 +1,11 @@
 package com.malgn.member.presentation.controller;
 
+import com.malgn.global.configure.CustomUser;
 import com.malgn.member.application.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +15,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class MemberController {
 
     private final MemberService memberService;
+
+    @DeleteMapping
+    public ResponseEntity<Void> deleteMember(
+        @AuthenticationPrincipal CustomUser user
+    ) {
+        memberService.deleteMember(user);
+        return ResponseEntity.noContent().build();
+    }
 
 
 }
