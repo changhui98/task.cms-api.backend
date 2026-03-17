ab29dc94fb712ce575e8dd71c95e263d2c44db2d
changhui98 <net_0102@naver.com>
2026-03-17 21:29:56 +0900

    Feat : 멤버 서비스 추가 기능을 위한 서비스 생성 및 의존 주입 설정

---
+++ src/main/java/com/malgn/member/application/service/MemberService.java
@@ -0,0 +1,13 @@
+package com.malgn.member.application.service;
+
+import com.malgn.member.domain.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+}
--- src/main/java/com/malgn/member/presentation/controller/MemberController.java
+++ src/main/java/com/malgn/member/presentation/controller/MemberController.java
@@ -1,11 +1,16 @@
 package com.malgn.member.presentation.controller;
 
+import com.malgn.member.application.service.MemberService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/members")
+@RequiredArgsConstructor
 public class MemberController {
+
+    private final MemberService memberService;
 
 
 }
